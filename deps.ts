5b668fe9deb5a4677f3e295411c5a72051b339ad
Siddharth Singh <me@shantaram.xyz>
2024-06-02 19:07:44 +0530

    update to kysely 0.27 and deno-postgres 0.19

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * as kysely from 'https://esm.sh/kysely@0.17.1/dist/esm/index-nodeless.js';
-export * as postgres from 'https://deno.land/x/postgres@v0.17.0/mod.ts';
+export * as kysely from 'npm:kysely@0.27.3';
+export * as postgres from 'https://deno.land/x/postgres@v0.19.0/mod.ts';
--- src/PostgreSQLDriverDatabaseConnection.ts
+++ src/PostgreSQLDriverDatabaseConnection.ts
@@ -9,7 +9,9 @@ export class PostgreSQLDriverDatabaseConnection
     this.client = options.client;
     this.transaction = null;
   }
-
+  streamQuery<R>(_compiledQuery: kysely.CompiledQuery<unknown>, _chunkSize?: number | undefined): AsyncIterableIterator<kysely.QueryResult<R>> {
+    throw new Error();
+  }
   async executeQuery<T>(compiledQuery: kysely.CompiledQuery) {
     return await (this.transaction ?? this.client).queryObject<T>(
       compiledQuery.sql,
